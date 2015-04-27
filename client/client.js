22476048883ee431b72392d42edd0d8b132c386a
Alex Brown <browna95@gmail.com>
2015-04-26 23:15:41 -0400

    Added code to remove orphaned trips.

--- client/client.js
+++ client/client.js
@@ -1,7 +1,8 @@
 console.log("== client.js " )
 
 Template.tripsTemplate.helpers({
-  trips: function() { return Trips.find({}) }
+  trips: function() { return Trips.find({}) },
+  tripCount: function() { return Trips.find({}).count() }
 });
 
 
@@ -12,31 +13,61 @@ Meteor.startup(function() {
 // Trip id to marker associative map.
 var tripMarkerMap = {};
 
+function getVehicleIcon(bearing) {
+  return {
+    path: 'M 11 0 L 16 11 L 16 22 L 5 22 L 5 11 z',
+    scale: 0.7,
+    fillColor: '#668866',
+    strokeColor: '#000000',
+    fillOpacity: 1,
+    rotation: parseFloat(bearing),
+    anchor: new google.maps.Point(11, 11)
+  }
+}
+
 function updateVehicleMarker(id, fields) {
-  console.log("=== updateVehicleMarker: ")
-  console.log("   vehicle=" + JSON.stringify(fields));
+  //console.log("=== updateVehicleMarker: ")
+  //console.log("   vehicle=" + JSON.stringify(fields));
 
 
   if(tripMarkerMap[id] === undefined) {
-    console.log("=== updateVehicleMarker: not found")
+    //console.log("=== updateVehicleMarker: not found")
+
+    var map = GoogleMaps.maps.vehicleMap.instance;
 
     // We haven't seen this trip yet, create a marker for the vehicle.
     var latLng = new google.maps.LatLng(parseFloat(fields.vehicle.vehicle_lat), parseFloat(fields.vehicle.vehicle_lon));
     var marker = new google.maps.Marker({
       position: latLng,
       title: fields.trip_headsign,
-      map: GoogleMaps.maps.vehicleMap.instance
+      map: map,
+      icon: getVehicleIcon(fields.vehicle.vehicle_bearing),
+      // icon: {
+      //   path: google.maps.SymbolPath.CIRCLE,
+      //   scale: 3
+      // },
     });
+
+    var infowindow = new google.maps.InfoWindow({
+      content: "<b>" + fields.trip_headsign + " </b>" + fields.vehicle.vehicle_id + "<br/>" + fields.trip_name
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map, marker);
+    });    
 
     tripMarkerMap[id] = marker;
   }
   else {
     // Update the marker.
-    console.log("=== updateVehicleMarker: found")
+    //console.log("=== updateVehicleMarker: found")
 
     if(fields.vehicle.vehicle_lat !== undefined && fields.vehicle.vehicle_lon !== undefined) {
       var latLng = new google.maps.LatLng(parseFloat(fields.vehicle.vehicle_lat), parseFloat(fields.vehicle.vehicle_lon));
       tripMarkerMap[id].setPosition(latLng);
+    }
+    if(fields.vehicle.vehicle_bearing !== undefined) {
+      tripMarkerMap[id].setIcon(getVehicleIcon(fields.vehicle.vehicle_bearing));
     }
   }
 }
@@ -44,17 +75,17 @@ function updateVehicleMarker(id, fields) {
 function observeTrips() {
   Trips.find({}).observeChanges({
     added: function (id, fields) {
-      console.log("== observe: Trip added " + id)
+      console.log("== observe: Trip added   " + fields.vehicle.vehicle_id + " id:" + id)
       if(fields.vehicle !== undefined)
         updateVehicleMarker(id, fields);
     },
     changed: function (id, fields) {
-      console.log("== observe: Trip updated")
+      console.log("== observe: Trip updated " + fields.vehicle.vehicle_id + " id:" + id)
       if(fields.vehicle !== undefined)
         updateVehicleMarker(id, fields);
     },
     removed: function (id) {
-      console.log("== observe: Trip removed")
+      console.log("== observe: Trip REMOVED " + id)
       if(tripMarkerMap[id] !== undefined) {
         tripMarkerMap[id].setMap(null);
         delete tripMarkerMap[id];
@@ -68,6 +99,7 @@ Template.body.helpers({
     // Make sure the maps API has loaded
     if (GoogleMaps.loaded()) {
       console.log("==mapOptions loaded")
+
       // Map initialization options
       return {
         center: new google.maps.LatLng(42.34004, -71.16006),
@@ -111,6 +143,9 @@ Template.body.onCreated(function() {
   console.log("==onCreated ")
   // We can use the `ready` callback to interact with the map API once the map is ready.
   GoogleMaps.ready('vehicleMap', function(map) {
+    var transitLayer = new google.maps.TransitLayer();
+    transitLayer.setMap(map.instance);
+
     observeTrips();
   });
 });
--- server/server.js
+++ server/server.js
@@ -10,41 +10,61 @@ Meteor.startup(function () {
 
   Trips.remove({});
 
-  var useTimer = false;
+  var useTimer = true;
   if(useTimer) {
+    getVehiclesByRoute('Green-C');
+    getVehiclesByRoute('Green-D');
     Meteor.setInterval(function() {
       getVehiclesByRoute('Green-D');
-      getVehiclesByRoute('Green-B');
+      getVehiclesByRoute('Green-C');
+      // getVehiclesByRoute('Green-B');
     }, 10000);
   }
   else {
     getVehiclesByRoute('Green-D');
-    getVehiclesByRoute('Green-B');
+    //getVehiclesByRoute('Green-B');
   }
 });
-
 
 function getVehiclesByRoute(routeId) {
   var url = MBTA_API_ROOT_URL + "vehiclesbyroute" + "?api_key=" + MBTA_API_KEY + "&route=" + routeId + "&format=json";
   
+  console.log("====")
   HTTP.get(url,
     function (error, result) {
       if (!error) {
         var content = JSON.parse(result.content);
-        console.log('Got it: ' + JSON.stringify(content, null, 2));
+        //console.log('Got it: ' + JSON.stringify(content, null, 2));
 
-        var routeName = content.route_name;
-        console.log('==routeName = ' + routeName);
+        var oldTripIds = Trips.find({route_id: content.route_id}).map(function(trip) {
+          return trip.trip_id;
+        });
+        var newTripIds = [];
 
         content.direction.forEach(function(direction) {
-          console.log('  ' + direction.direction_name);
-
           direction.trip.forEach(function(trip) {
-            console.log('            vehicle= ' + trip.vehicle.vehicle_id + '  ' + trip.vehicle.vehicle_lat + '  ' + trip.vehicle.vehicle_lon)
+            trip.route_id = content.route_id;
+            console.log('            ' + trip.trip_id + ' vehicle= ' + trip.vehicle.vehicle_id + ' ' + content.route_id)
 
             Trips.upsert({ trip_id: trip.trip_id }, trip, { upsert: true });
+            newTripIds.push(trip.trip_id);
           })
         });
+
+        console.log('oldTripIds: ' + JSON.stringify(oldTripIds))
+        console.log('newTripIds: ' + JSON.stringify(newTripIds))
+
+        // Remove trips that were not included in this update.  Assume they've either gone
+        // underground or have been taken out of service etc...
+        oldTripIds.forEach(function(trip_id) {
+          if(newTripIds.indexOf(trip_id) === -1) {
+            console.log('--Removing: ' + trip_id)
+            Trips.remove({trip_id: trip_id})
+          }
+        })
+      }
+      else {
+        console.log("API request error: " + error)
       }
     });
 }
